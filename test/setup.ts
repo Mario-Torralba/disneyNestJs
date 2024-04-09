8719e83288438424ce1df305eada88cb1b336487
Mario-Torralba <torralba1995@gmail.com>
2024-04-09 19:10:21 +0200

    .

--- test/setup.ts
+++ test/setup.ts
@@ -1,4 +1,4 @@
 // test/setup.ts
 
 // Configuraciones globales para Jest, por ejemplo:
-jest.setTimeout(10000); // Ajusta el tiempo de espera para las pruebas
+jest.setTimeout(10); // Ajusta el tiempo de espera para las pruebas
